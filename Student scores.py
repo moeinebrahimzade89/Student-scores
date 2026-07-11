c75ab4649bfe9b257914efd9bf9c96ca71b0fefc
moeinebrahimzade89 <moein.ebrahimzade.89@gmail.com>
2026-07-11 12:01:34 +0330

    first commit

---
+++ Student scores.py
@@ -0,0 +1,5 @@
+Student = ["Amir" ,"Mohammad" ,"Moein" ,"Mobin" ,"Amirhosein" ,"Mina" ,"Narges" ,"Elnaz"]
+scores = [2 ,3 ,4 ,5 ,4 ,3 ,7 ,3]
+
+for i in range(len(Student)):
+    print(Student[i] , scores[i] * "*" , sep= "")
