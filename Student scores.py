40351205416b9c5e87cc4f16d30d20c76957db83
Moein Ebrahimzade <moein.ebrahimzade.89@gmail.com>
2026-07-12 21:08:05 +0330

    Add a comment Student scores.py

--- Student scores.py
+++ Student scores.py
@@ -1,5 +1,7 @@
-Student = ["Amir", "Mohammad", "Moein", "Mobin", "Amirhosein", "Mina", "Narges", "Elnaz"]
-scores = [2, 3, 4, 5, 4, 3, 7, 3]
+# The lists must correspond.
+Student = ["Amir" ,"Mohammad" ,"Moein" ,"Mobin" ,"Amirhosein" ,"Mina" ,"Narges" ,"Elnaz"]
+scores = [2 ,3 ,4 ,5 ,4 ,3 ,7 ,3]
 
 for i in range(len(Student)):
-    print(Student[i], scores[i] * "*", sep="")
+    # It prints the names of the students and their scores in front of them.
+    print(Student[i] , scores[i] * "*" , sep= "")
